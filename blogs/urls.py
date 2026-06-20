12fa5935be9c642474608b486e60168320940e9b
Ashutosh Kumar Mantoo <ashumantoo@gmail.com>
2026-06-20 12:49:35 +0530

    category link

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from blogs import views
 
 urlpatterns = [
-    path('<int:category_id>/', views.posts_by_category, name='post_by_category')
+    path('<int:category_id>/', views.posts_by_category, name='posts_by_category')
 ]

4f1672003a7ed3524b7550ae58c0c6476e5b89b3
Ashutosh Kumar Mantoo <ashumantoo@gmail.com>
2026-06-21 01:04:45 +0530

    Dashboard Post CRUD

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 
 class CategoryForms(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BlogPostForms(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title','category','featured_image','short_description','blog_body','status','is_featured')
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.defaultfilters import slugify
 
 from blogs.models import Category, Blog
-from dashboards.forms import CategoryForms
+from dashboards.forms import CategoryForms, BlogPostForms
 
 
 #if user not logged in then django will redirect user to login since we have passed login_url
@@ -56,3 +57,58 @@ def delete_category(request,pk):
     category = get_object_or_404(Category,pk=pk)
     category.delete()
     return redirect('categories')
+
+
+def posts(request):
+    dashboard_posts = Blog.objects.all()
+
+    context = {
+        'posts': dashboard_posts
+    }
+    return render(request,'dashboard/posts.html',context)
+
+
+def add_post(request):
+    if request.method == 'POST':
+        #In request.POST: We receive the data like: title, category, status, short_description, blog_body etc
+        #But the image will be received in request.FILES filed
+        form = BlogPostForms(request.POST, request.FILES)
+        if form.is_valid():
+            temp_post = form.save(commit=False) #temporarily saving the form data
+            temp_post.author = request.user
+            temp_post.save() #saving the post here because I have to use the newly created post Id(pk) in below line
+            temp_post.slug = slugify(form.cleaned_data['title'] + '-'+str(temp_post.id))
+            temp_post.save()
+            return redirect('posts')
+    else:
+        form = BlogPostForms()
+
+    context = {
+        'form': form
+    }
+    return render(request,'dashboard/add_post.html', context)
+
+def edit_post(request,pk):
+    post = get_object_or_404(Blog,pk=pk)
+    if request.method == 'POST':
+        form = BlogPostForms(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            updated_post = form.save()
+            title = form.cleaned_data['title'] + '-'+str(updated_post.id)
+            #Need to generate the slug again
+            updated_post.slug = slugify(title)
+            updated_post.save()
+            return redirect('posts')
+
+    form = BlogPostForms(instance=post) #Populating data in the form to edit
+    context = {
+        'form': form,
+        'post': post
+    }
+    return render(request,'dashboard/edit_post.html',context)
+
+
+def delete_post(request,pk):
+    post = get_object_or_404(Blog,pk=pk)
+    post.delete()
+    return redirect('posts')
